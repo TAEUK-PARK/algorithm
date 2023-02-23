d6425c4963b5b3eb91201de047905a02caae2846
taeuk <dcjimin@gmail.com>
2023-02-23 21:02:10 +0900

    230223

---
+++ 2. YELLOW/[230223]Smallest Range Covering Elements from K Lists/blank.js
@@ -0,0 +1,8 @@
+/**
+ * 난이도: HARD
+ * https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists
+ *
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+const smallestRange = function (nums) {};
